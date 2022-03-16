0aef1e731e512dadb52bd8bf039cee3ef09a5557
tanyaleepr <tanyaleepr@gmail.com>
2022-03-16 14:53:00 -0500

    files created

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
+
+
+// this is a comment example
