f364e65162e36b19edd42e5b0d86ec71ce267ef7
tanyaleepr <tanyaleepr@gmail.com>
2022-03-18 18:21:22 -0500

    2

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: '0.0.0.0',
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3000
+      port: 3306
     });
 
 module.exports = sequelize;
